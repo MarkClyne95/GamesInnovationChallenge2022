972e4c193d3ec1b61a49313cfd1c639d500ea284
ScottEwing191 <73129765+ScottEwing191@users.noreply.github.com>
2022-02-27 18:16:53 +0000

    Started River Garbage
    Not working right yet. The garbage sort of forms two lines one on either side of the river

---
+++ Assets/Scripts/River/Buoyancy_S.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GIC.River {
+    [RequireComponent(typeof(Rigidbody))]
+    public class Buoyancy_S : MonoBehaviour {
+        Rigidbody rb;                                                       // Boat
+        [SerializeField] float upForce = 500.0f;                            // Buoyancy force
+        [SerializeField] Transform waterTransform;                          // Water
+        float waterLevelY;                                                  // Water level y component
+        [SerializeField] Transform buoyancyTransform;                       // Force is added at this position   
+
+        private void Start() {
+            rb = GetComponent<Rigidbody>();
+        }
+
+        private void FixedUpdate() {
+            waterLevelY = waterTransform.position.y;
+            if (transform.position.y < waterTransform.position.y) {
+                float dst = Vector3.Distance(transform.position, new Vector3(transform.position.x, waterLevelY, transform.position.z));
+                rb.AddForceAtPosition(Vector3.up * dst * upForce, buoyancyTransform.position, ForceMode.Acceleration);
+            }
+        }
+
+
+
+
+
+    } 
+}
